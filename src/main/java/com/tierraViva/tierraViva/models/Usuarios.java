b735242b30a207e72b3e181c40cb65fc1cfd6452
Maggievira87 <margarethjvr87@gmail.com>
2026-01-13 17:16:18 -0500

    Cambios en usuario

--- src/main/java/com/tierraViva/tierraViva/models/Usuarios.java
+++ src/main/java/com/tierraViva/tierraViva/models/Usuarios.java
@@ -1,4 +1,86 @@
 package com.tierraViva.tierraViva.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuarios {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuarios")
+    private Integer idUsuarios;
+
+    private String nombre;
+    private String usuario;
+    private String email;
+    private Long numeroCelular;
+
+    @Column(name = "contraseña")
+    private String contrasena;
+
+    public Usuarios() {
+    }
+
+    public Usuarios(String nombre, String usuario, String email, Long numeroCelular, String contrasena) {
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.email = email;
+        this.numeroCelular = numeroCelular;
+        this.contrasena = contrasena;
+    }
+
+
+    public Integer getIdUsuarios() {
+        return idUsuarios;
+    }
+
+    public void setIdUsuarios(Integer idUsuarios) {
+        this.idUsuarios = idUsuarios;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getNumeroCelular() {
+        return numeroCelular;
+    }
+
+    public void setNumeroCelular(Long numeroCelular) {
+        this.numeroCelular = numeroCelular;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
 }
