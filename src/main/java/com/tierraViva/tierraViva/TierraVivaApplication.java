a8bc7abd8ca9962205b9c9cfac3a9acc9a2eaf76
Daniel Suarez <danfecisf@gmail.com>
2026-01-13 16:26:25 -0500

    Primer commit estructura springboot

---
+++ src/main/java/com/tierraViva/tierraViva/TierraVivaApplication.java
@@ -0,0 +1,13 @@
+package com.tierraViva.tierraViva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TierraVivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TierraVivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tierraViva/tierraViva/TierraVivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tierraViva.tierraViva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TierraVivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
