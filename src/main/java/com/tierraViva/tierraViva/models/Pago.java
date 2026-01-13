0f8974d90bd62f5356a1c78c7681e63465f38251
Dan802 <jfelipeg05@gmail.com>
2026-01-13 17:12:48 -0500

    Pago model structure

--- src/main/java/com/tierraViva/tierraViva/models/Pago.java
+++ src/main/java/com/tierraViva/tierraViva/models/Pago.java
@@ -1,4 +1,65 @@
 package com.tierraViva.tierraViva.models;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "pagos")
 public class Pago {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_pago;
+
+    // Foreign key
+    private Long id_carrito;
+
+    private String metodoPago;
+    private Date fechaPago;
+    private BigDecimal monto;
+
+    public Pago() {
+    }
+
+    public Pago(Long id_pago, Long id_carrito, String metodoPago, Date fechaPago, BigDecimal monto) {
+        this.id_pago = id_pago;
+        this.id_carrito = id_carrito;
+        this.metodoPago = metodoPago;
+        this.fechaPago = fechaPago;
+        this.monto = monto;
+    }
+
+    public Long getId_pago() {
+        return id_pago;
+    }
+
+    public void setId_pago(Long id_pago) {
+        this.id_pago = id_pago;
+    }
+
+    public String getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(String metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public Date getFechaPago() {
+        return fechaPago;
+    }
+
+    public void setFechaPago(Date fechaPago) {
+        this.fechaPago = fechaPago;
+    }
+
+    public BigDecimal getMonto() {
+        return monto;
+    }
+
+    public void setMonto(BigDecimal monto) {
+        this.monto = monto;
+    }
 }
