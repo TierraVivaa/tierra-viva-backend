d55ae775bc95afeb98ef357fbc078cdc6f12c22b
Daniel Suarez <danfecisf@gmail.com>
2026-01-24 21:56:56 -0500

    ajuste securityconfig para login

--- src/main/java/com/tierraViva/tierraViva/SecurityConfig.java
+++ src/main/java/com/tierraViva/tierraViva/SecurityConfig.java
@@ -29,8 +29,16 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Sin sesiones (JWT)
                 .authorizeHttpRequests(auth -> auth
                         // Permitimos el registro de usuarios y el login (cuando lo crees) sin token
-                        .requestMatchers(HttpMethod.GET, "/usuarios/**", "/productos/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/usuarios/**", "/productos/**").permitAll()
+                        .requestMatchers(
+                                HttpMethod.GET,
+                                "/usuarios/**",
+                                "/productos/**",
+                                "/api/categorias/**"   //
+                        ).permitAll()
+                        .requestMatchers(HttpMethod.POST,
+                                "/usuarios/**",
+                                "/auth/loginConDTO",
+                                "/productos/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/login").permitAll()
                         // Cualquier otra petición requerirá autenticación
                         .anyRequest().authenticated()
